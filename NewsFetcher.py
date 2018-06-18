a6237ae848113838386e20f4a22bff581395e728
Seth J. Connell <sethjordanconnell@gmail.com>
2018-06-18 18:40:11 -0500

    init

---
+++ NewsFetcher.py
@@ -0,0 +1,9 @@
+#Made some imports I'll need later on.
+import socialshares
+import requests
+import ast
+import unicodedata
+from bs4 import BeautifulSoup
+from six.moves import urllib
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
---
+++ README.md
@@ -0,0 +1,1 @@
+A tool to extract breaking news stories from some of the top political websites in the world.
