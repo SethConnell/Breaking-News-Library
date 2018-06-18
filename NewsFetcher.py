b0ee9d31c53445511bfbdb1d173a602bb536f47e
Seth J. Connell <sethjordanconnell@gmail.com>
2018-06-18 18:45:51 -0500

    init

--- NewsFetcher.py
+++ NewsFetcher.py
@@ -7,3 +7,19 @@ from bs4 import BeautifulSoup
 from six.moves import urllib
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+
+#A simple function to get number of shares on Facebook of a url using social shares module.
+def facebookShares(url):
+    counts = socialshares.fetch(url, ['facebook'])
+    return int(counts['facebook']['share_count'])
+
+stories = []
+
+# Using this function to display data saved as dicts to the list 'stories'.
+def displayStories():
+    global stories
+    for i in range(0, len(stories)):
+        print stories[i]["headline"]
+        print stories[i]['url']
+        print stories[i]['shares']
+        print ""
