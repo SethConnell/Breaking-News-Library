adaf52f1960012a68cd4d5e13d392407e4980c05
Seth J. Connell <sethjordanconnell@gmail.com>
2018-06-18 19:01:04 -0500

    init

--- NewsFetcher.py
+++ NewsFetcher.py
@@ -23,3 +23,23 @@ def displayStories():
         print stories[i]['url']
         print stories[i]['shares']
         print ""
+
+#A clean function that scrapes Fox News.com        
+def scrapeFoxNews():
+    global stories
+    foxnews = "http://www.foxnews.com/"
+    r  = requests.get(foxnews)
+    data = r.text
+    soup = BeautifulSoup(data,"lxml")
+    for i in range(0, 15):
+        foundstories = soup.find_all("article", class_="article story-" + str(i))
+        for data in foundstories:
+            htmlatag = data.find("h2", class_="title").find("a")
+            headline = htmlatag.getText()
+            url = htmlatag.get("href")
+            shares = facebookShares(url)
+            d = {"headline" : headline,
+                 "url" : url,
+                 "shares" : shares}
+            stories.append(d)
+
